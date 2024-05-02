6f853ba3133aa0d7c4b888903f0035dc8f011c4e
Famak2003 <Famaki310@gmail.com>
2024-05-02 20:53:06 +0300

    initiated menu animation effect

--- src/router/HomeLayout.jsx
+++ src/router/HomeLayout.jsx
@@ -19,26 +19,55 @@ function Home() {
     });
   };
   return (
-    <div className=" flex flex-col h-fit overflow-hidden ">
-      <div className=" h-[15svh] flex justify-between items-center generalPadding ">
-        <button className=" " onClick={() => setDispayMenu(!dispayMenu)}>
-          <img src={STARGGERDMENU} alt="menu" />
-        </button>
-        <h1 className=" text-[2rem]">ChatAI</h1>
-        <button>
-          <img src={DOTTEDMENU} alt="menu" />
-        </button>
-      </div>
+    <div className="flex flex-col h-fit overflow-hidden ">
       <div
         className={` home-container flex w-fit ${
-          dispayMenu ? "translate-x-0" : "translate-x-[-100vw]"
+          dispayMenu ? "translate-x-0" : "translate-x-[-90vw]"
         } duration-700 `}
       >
         {/* // Menu // */}
-        <div className=" w-screen bg-red-800 generalPadding ">Hello</div>
+        <div className=" w-[90vw] bg-custom-black pt-[10rem] generalPadding ">
+          Today
+        </div>
         {/* // Outlet // */}
-        <div className=" w-screen rotate-[0deg] ">
-          <Outlet />
+        <div
+          className={` box flex flex-col relative h-fit w-screen  ${
+            dispayMenu ? "rotate-[-10deg] rounded-3xl " : "rotate-0"
+          } delay-500 duration-300  `}
+        >
+          <div className=" z-[10] bg-custom-black rounded-3xl ">
+            <div
+              className={` w-full h-[15svh] flex justify-between items-center generalPadding `}
+            >
+              <button className=" " onClick={() => setDispayMenu(!dispayMenu)}>
+                <img src={STARGGERDMENU} alt="menu" />
+              </button>
+              <h1 className=" text-[2rem]">ChatAI</h1>
+              <button className="  ">
+                <img src={DOTTEDMENU} alt="menu" />
+              </button>
+            </div>
+
+            <div>
+              <Outlet />
+            </div>
+          </div>
+          {/* // Effects start // */}
+          <div
+            className={` absolute inset-0 z-[-1] bg-charcoal-gray ${
+              dispayMenu ? " sm:rotate-[-3deg] rotate-[-4deg] rounded-3xl" : ""
+            }  delay-[550ms] duration-300 `}
+          >
+            {" "}
+          </div>
+          <div
+            className={` absolute inset-0 z-[-2]  bg-light-gray ${
+              dispayMenu ? " sm:rotate-[-5deg] rotate-[-8deg] rounded-3xl" : ""
+            }  delay-[600ms] duration-300 `}
+          >
+            {" "}
+          </div>
+          {/* // Effects end // */}
         </div>
       </div>
 
